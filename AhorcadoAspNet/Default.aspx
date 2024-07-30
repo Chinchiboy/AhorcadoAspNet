d34ead51ba3897ee96a401a215728d96d76ac53c
Chinchiboy <ivan.espinosa.joliver@gmail.com>
2024-07-30 10:48:41 +0200

    Ahorcado con imagen y teclado en pantalla

--- AhorcadoAspNet/Default.aspx
+++ AhorcadoAspNet/Default.aspx
@@ -1,44 +1,21 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AhorcadoAspNet._Default" %>
+﻿<%@ Page Title="Juego de Ahorcado" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AhorcadoAspNet.Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-
     <main>
-        <section class="row" aria-labelledby="aspnetTitle">
-            <h1 id="aspnetTitle">ASP.NET</h1>
-            <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-            <p><a href="http://www.asp.net" class="btn btn-primary btn-md">Learn more &raquo;</a></p>
-        </section>
-
-        <div class="row">
-            <section class="col-md-4" aria-labelledby="gettingStartedTitle">
-                <h2 id="gettingStartedTitle">Getting started</h2>
-                <p>
-                    ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-                A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-                </p>
-                <p>
-                    <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-                </p>
-            </section>
-            <section class="col-md-4" aria-labelledby="librariesTitle">
-                <h2 id="librariesTitle">Get more libraries</h2>
-                <p>
-                    NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-                </p>
-                <p>
-                    <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-                </p>
-            </section>
-            <section class="col-md-4" aria-labelledby="hostingTitle">
-                <h2 id="hostingTitle">Web Hosting</h2>
-                <p>
-                    You can easily find a web hosting company that offers the right mix of features and price for your applications.
-                </p>
-                <p>
-                    <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-                </p>
-            </section>
+        <div class="container">
+            <h1>Juego de Ahorcado</h1>
+            <asp:Literal ID="wordContainer" runat="server"></asp:Literal>
+            <asp:Image ID="hangmanImage" runat="server" ImageUrl="~/img/Ahorcado0.jpg" />
+            <div class="alphabet-buttons">
+                <asp:Repeater ID="AlphabetRepeater" runat="server">
+                    <ItemTemplate>
+                        <asp:Button ID="btnLetter" runat="server" Text='<%# Eval("Letter") %>' OnClick="LetterButton_Click" />
+                    </ItemTemplate>
+                </asp:Repeater>
+            </div>
+            <asp:Label ID="messageLabel" runat="server" Text=""></asp:Label>
+            <asp:Button ID="restartButton" runat="server" Text="Reiniciar" OnClick="RestartButton_Click" Visible="false" />
+            <asp:Literal ID="wrongLettersContainer" runat="server"></asp:Literal>
         </div>
     </main>
-
 </asp:Content>
--- AhorcadoAspNet/Default.aspx.cs
+++ AhorcadoAspNet/Default.aspx.cs
@@ -1,17 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace AhorcadoAspNet
 {
-    public partial class _Default : Page
+    public partial class Default : Page
     {
+        private static readonly string[] Words = { "javascript", "programacion", "ahorcado", "desarrollo", "html", "css" };
+
+        private string SelectedWord
+        { get { return (string)ViewState["SelectedWord"]; } set { ViewState["SelectedWord"] = value; } }
+
+        private List<char> CorrectLetters
+        { get { return (List<char>)ViewState["CorrectLetters"] ?? new List<char>(); } set { ViewState["CorrectLetters"] = value; }}
+
+        private List<char> WrongLetters
+        {get { return (List<char>)ViewState["WrongLetters"] ?? new List<char>(); }set { ViewState["WrongLetters"] = value; }}
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                InitializeGame();
+                BindAlphabetRepeater();
+            }
+            DisplayWord();
+            DisplayWrongLetters();
+            UpdateHangmanImage();
+        }
 
+        private void InitializeGame()
+        {
+            Random random = new Random();
+            SelectedWord = Words[random.Next(Words.Length)];
+            CorrectLetters = new List<char>();
+            WrongLetters = new List<char>();
+            messageLabel.Text = string.Empty;
+            restartButton.Visible = false;
+            EnableAlphabetButtons(true);
+            UpdateHangmanImage();
+        }
+
+        private void BindAlphabetRepeater()
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".Select(c => new { Letter = c }).ToList();
+            AlphabetRepeater.DataSource = alphabet;
+            AlphabetRepeater.DataBind();
+        }
+
+        protected void LetterButton_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            char guessedLetter = b.Text[0];
+            if (!CorrectLetters.Contains(guessedLetter) && !WrongLetters.Contains(guessedLetter))
+            {
+                if (SelectedWord.Contains(guessedLetter.ToString()))
+                {
+                    CorrectLetters.Add(guessedLetter);
+                    messageLabel.Text = "¡Correcto!";
+                }
+                else
+                {
+                    WrongLetters.Add(guessedLetter);
+                    messageLabel.Text = "¡Incorrecto!";
+                }
+                DisplayWord();
+                DisplayWrongLetters();
+                CheckGameStatus();
+                b.Enabled = false;
+                UpdateHangmanImage();
+            }
+        }
+
+        private void DisplayWord()
+        {
+            wordContainer.Text = string.Join(" ", SelectedWord.ToCharArray().Select(letter => CorrectLetters.Contains(letter) ? letter.ToString() : "_"));
+        }
+
+        private void DisplayWrongLetters()
+        {
+            wrongLettersContainer.Text = $"Letras incorrectas: {string.Join(", ", WrongLetters)}";
+        }
+
+        private void CheckGameStatus()
+        {
+            if (WrongLetters.Count >= 6)
+            {
+                messageLabel.Text = $"¡Perdiste! La palabra era \"{SelectedWord}\".";
+                EnableAlphabetButtons(false);
+                restartButton.Visible = true;
+            }
+            else if (SelectedWord.All(letter => CorrectLetters.Contains(letter)))
+            {
+                messageLabel.Text = "¡Ganaste!";
+                EnableAlphabetButtons(false);
+                restartButton.Visible = true;
+            }
+        }
+
+        private void UpdateHangmanImage()
+        {
+            hangmanImage.ImageUrl = $"~/img/ahorcado{WrongLetters.Count}.jpg";
+        }
+
+        protected void RestartButton_Click(object sender, EventArgs e)
+        {
+            InitializeGame();
+            BindAlphabetRepeater();
+        }
+
+        private void EnableAlphabetButtons(bool enable)
+        {
+            foreach (RepeaterItem item in AlphabetRepeater.Items)
+            {
+                Button b = (Button)item.FindControl("btnLetter");
+                if (b != null)  b.Enabled = enable;
+            }
         }
     }
 }
